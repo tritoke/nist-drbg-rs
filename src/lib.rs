4eaa8269759081af120524cb4b77abf2c43fe92a
Sam Leonard <samuelleonard35@gmail.com>
2025-03-03 22:39:25 +0000

    Create initial sketch of Drbg trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![no_std]
+
+use core::marker::Sized;
+
+pub enum SeedError {
+    InsufficientEntropy,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub trait Drbg: Sized {
+    fn seed(entropy: &[u8]) -> Result<Self, SeedError>;
+    fn reseed(&mut self, entropy: &[u8]) -> Result<(), SeedError>;
+    fn random_bytes(&mut self, buf: &mut [u8]);
 }
