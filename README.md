c8161baa1e3c0af9bba68ecbcb565d10e5f6a8a4
Giacomo Pope <giacomopope@gmail.com>
2025-03-04 10:12:25 +0000

    some notes on the trait

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # nist-drbg-rs
-A Rust implementation of NIST SP 800-90A Rev. 1.
+A Rust implementation of [NIST SP 800-90A Rev. 1](https://csrc.nist.gov/pubs/sp/800/90/a/r1/final).
--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,26 @@ pub enum SeedError {
     InsufficientEntropy,
 }
 
+// NOTES:
+// seed (called instantiate in the NIST spec)
+// nonce is needed for HMAC and HASH but not CTR, maybe we don't include this for Drbg?
+// personalization_string is basically optional context bytes to add into seeding
+// security strength is an int which is optional and defaults to 32 when missing I believe
+//
+// reseed
+// like personalization_string above, additional_input are optional context bytes you can
+// feed into the reseed
+//
+// random_bytes (generate in the NIST spec)
+// NOTE: in the NIST api you request a number of BITS, but this API fills a buffer of bytes
+// again, additional_input are optional bytes to feeding into the generate function
 pub trait Drbg: Sized {
-    fn seed(entropy: &[u8]) -> Result<Self, SeedError>;
-    fn reseed(&mut self, entropy: &[u8]) -> Result<(), SeedError>;
-    fn random_bytes(&mut self, buf: &mut [u8]);
+    fn seed(
+        entropy: &[u8],
+        nonce: Option(&[u8]),
+        personalization_string: Option(&[u8]),
+        security_strength: Option(u8),
+    ) -> Result<Self, SeedError>;
+    fn reseed(&mut self, entropy: &[u8], additional_input: Option(&[u8])) -> Result<(), SeedError>;
+    fn random_bytes(&mut self, buf: &mut [u8], additional_input: Option(&[u8]));
 }
