2064cc8957f41add30308f2c5494762131adaf20
Giacomo Pope <giacomopope@gmail.com>
2025-03-05 11:32:51 +0000

    sketch some ideas for instantiate:

--- src/hash.rs
+++ src/hash.rs
@@ -17,6 +17,7 @@ pub struct HashDrbg<H: Digest, const SEEDLEN_BYTES: usize, const HSSS: u32> {
     reseed_counter: u64,
 
     // admin bits - not sure about these right now but the standard shows them
+    // Jack: NIST doc says security_strength is optional for Hash_DRBG as it is unused.
     security_strength: u32,           // ?? ig
     prediction_resistance_flag: bool, // is this drbg prediction resistant?
 
@@ -37,10 +38,54 @@ impl Error for InsufficientEntropyError {}
 impl<H: Digest, const SEEDLEN: usize, const HSSS: u32> HashDrbg<H, SEEDLEN, HSSS> {
     pub fn new(
         _entropy: &[u8],
-        _nonce: u64,
-        _personalisation_string: &[u8],
+        _nonce: &[u8], // Jack: the nonce can be up to 128 bits (half security bits)
+        _personalisation_string: &[u8], // this can have length zero, do we want empty slice or Option?
     ) -> Result<Self, InsufficientEntropyError> {
         todo!()
+
+        // Rough idea based on my reading of the spec
+        // NOTE: not sure the nicest was to concat slices in no_std
+        // seed_material = entropy || nonce || personalisation_string
+        // self.value = hash_df(seed_material, SEEDLEN_BYTES)
+        // self.constant = hash_df(0x00 | V, SEEDLEN_BYTES)
+        // self.reseed_counter = 1
+    }
+
+    /// Auxiliary function defined in 10.3.1
+    fn hash_df(_seed_material: &[u8], byte_count: u32) -> Result<&[u8], LengthError> {
+        todo!()
+
+        // Compute the length, this needs to know the output size of
+        // the hash function which im sure is available
+        hash_output_len: u32 = 32; // This is wrong!
+
+        // len = ceil(byte_count / hash_output_len)
+        len: u32 = (byte_count + hash_output_len - 1) / hash_output_len;
+        if len > 255 {
+            return Err; // len <= 255
+        }
+
+        // tmp should be null to begin with but will be extended...
+        // not sure the nicest as to do this in no_std
+        // The idea is we will generate len * hash_output_len bytes
+        // and we know this now, so I guess we can preallocate?
+        tmp: &[u8] = &[];
+
+        // Set an 8-bit counter to one to len
+        for for counter in 1..=len {
+            // We now want to append hash_output_len to tmp
+            hash_output: mut &[u8]; // no idea what this should be type wise...
+
+            // Here we compute the hash over counter as a u8, the number of output bits
+            // as a u32 and then the seed material passed into the function
+            // hash_output = Hash(counter || (byte_count * 8) as u32 || _seed_material)
+
+            // finally we append this hash output into tmp
+            // tmp = tmp || hash_output
+        }
+
+        // The output of Hash_df is the leftmost bytes of tmp
+        return OK(&tmp[..byte_count]);
     }
 }
 
