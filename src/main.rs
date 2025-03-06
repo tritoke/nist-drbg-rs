5ccb97fefb7fe6c17b4564fb5fba173580504a11
Giacomo Pope <giacomopope@gmail.com>
2025-03-06 18:27:09 +0000

    test with additional data

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,23 @@ fn test_hash() {
     println!("{}", buf == returned_bytes);
 }
 
+fn test_hash_add() {
+    let entropy: &[u8] = &hex::decode("c3ef82ce241f02e4298b118ca4f16225").unwrap();
+    let nonce: &[u8] = &hex::decode("15e32abbae6b7433").unwrap();
+    let add1: &[u8] = &hex::decode("2b790052f09b364d4a8267a0a7de63b8").unwrap();
+    let add2: &[u8] = &hex::decode("2ee0819a671d07b5085cc46aa0e61b56").unwrap();
+    let returned_bytes: &[u8] = &hex::decode("5825fa1d1dc33c64cdc8690682eff06039e79508c3af48e880f8227d5f9aaa14b3bc76baee477ebbb5c45547134179223257525e8f3afefb78b59da032f1006d74c9831375a677eab3239c94ebe3f7fa").unwrap();    let mut drbg = Sha1Drbg::new(
+        entropy,
+        nonce,
+        &[]
+    ).unwrap();
+
+    let mut buf: [u8; 80] = [0; 80];
+    let _ = drbg.random_bytes_extra(&mut buf, add1);
+    let _ = drbg.random_bytes_extra(&mut buf, add2);
+    println!("{}", buf == returned_bytes);
+}
+
 fn test_hmac() {
     let entropy: &[u8] = &hex::decode("e91b63309e93d1d08e30e8d556906875").unwrap();
     let nonce: &[u8] = &hex::decode("f59747c468b0d0da").unwrap();
@@ -36,7 +53,27 @@ fn test_hmac() {
     println!("{}", buf == returned_bytes);
 }
 
+fn test_hmac_add() {
+    let entropy: &[u8] = &hex::decode("32c1ca125223de8de569697f92a37c67").unwrap();
+    let nonce: &[u8] = &hex::decode("72d4cc4f0544d409").unwrap();
+    let add1: &[u8] = &hex::decode("9e98cc8e0f8eb84d1911c1775a5703bb").unwrap();
+    let add2: &[u8] = &hex::decode("593aa3a300e5c907a011dd5a3dcd77e2").unwrap();
+    let returned_bytes: &[u8] = &hex::decode("942909a9d380aa5d4e3af69093a8fa513ee545b9bf9e1b81c5f30966db3e5cb52f8b1b6fe440d592e5fe4a972c36aa498035e2442f82910c5cd095c7f4b4c7e7555c4669cca481cdfbfda167b5d6f8d5").unwrap();
+    let mut drbg = HmacSha1Drbg::new(
+        entropy,
+        nonce,
+        &[]
+    ).unwrap();
+
+    let mut buf: [u8; 80] = [0; 80];
+    let _ = drbg.random_bytes_extra(&mut buf, &add1);
+    let _ = drbg.random_bytes_extra(&mut buf, &add2);
+    println!("{}", buf == returned_bytes);
+}
+
 fn main() {
     test_hash();
+    test_hash_add();
     test_hmac();
+    test_hmac_add();
 }
